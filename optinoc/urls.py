4f168fe646341ab5ec92aebcea79d68e3cde9421
Erik Pope <erik.pope@gmail.com>
2025-06-28 14:53:41 -0700

    Configure static and media files

--- optinoc/urls.py
+++ optinoc/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -7,3 +9,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', views.index, name='index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
